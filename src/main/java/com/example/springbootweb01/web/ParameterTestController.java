8a124a64d6984fcc8925dc5446807ee9f574a17d
Bob Tang <254202042@qq.com>
2022-12-05 19:17:56 +0800

    常用请求注解的使用

--- src/main/java/com/example/springbootweb01/web/ParameterTestController.java
+++ src/main/java/com/example/springbootweb01/web/ParameterTestController.java
@@ -1,8 +1,6 @@
 package com.example.springbootweb01.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,14 +12,39 @@ public class ParameterTestController {
     // /car/{id}/owner/{username}
     @GetMapping("/car/{id}/owner/{username}")
     public Map<String,Object> getCar(
+            // 常用参数注解的使用
+            // 获取路径参数
             @PathVariable("id") Integer id,
             @PathVariable("username") String username,
-            @PathVariable Map<String,String> pv
-    ) {
+            @PathVariable Map<String,String> pv,
+            @RequestHeader("User-Agent")  String userAgent,
+            @RequestHeader Map<String,String> headers,
+            // 获取请求参数 interests
+            @RequestParam("interests") String[] interests,
+            // 获取所有的参数
+            @RequestParam Map<String,String> params
+            // 获取cookie
+            // @CookieValue("_ga") String _ga
+            // 获取请求体的值
+            ) {
         Map<String,Object> map = new HashMap<>();
         map.put("id",id);
         map.put("name",username);
         map.put("pv",pv);
+        map.put("User-Agent",userAgent);
+        map.put("headers",headers);
+        map.put("interests",interests);
+        map.put("params",params);
+        // map.put("_ga",_ga);
+        return map;
+    }
+
+    @PostMapping("/save")
+    public Map<String,Object> save(
+            @RequestBody Map<String,String> content
+    ) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("params",content);
         return map;
     }
 }
